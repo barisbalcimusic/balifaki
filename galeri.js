f1d2b92e5931121f813474748dcb3f897ffc20ac
Baris Balci <barisbalcimusic@gmail.com>
2024-01-01 15:05:54 +0100

    wont push

--- galeri.js
+++ galeri.js
@@ -1,14 +1,16 @@
 // GALERI
+
 for (let i = 1; i < 69; i++) {
   let bg = document.getElementById("bg-field");
   let field = document.getElementById("field");
   let exit = document.getElementById("exit");
 
-  let image = document.createElement("img");
-  document.getElementById("galeri-container").appendChild(image);
-  image.classList.add("galeri-img");
-  image.setAttribute("src", `./images/pictures/${i}.jpg`);
+  // let image = document.createElement("img");
+  // document.getElementById("galeri-container").appendChild(image);
+  // image.classList.add("galeri-img");
+  // image.setAttribute("src", `./images/pictures/${i}.jpg`);
 
+  let image = document.getElementsByClassName("galeri-img");
   image.addEventListener("click", show);
   bg.addEventListener("click", hide);
   exit.addEventListener("click", hide);
@@ -18,13 +20,15 @@ for (let i = 1; i < 69; i++) {
     field.classList.add("visible-field");
     document.body.style.overflowY = "hidden";
 
-    let imageData = document.createElement("img");
-    document.getElementById("img-field").appendChild(imageData);
-    imageData.classList.add("preview-img");
-    imageData.setAttribute("id", "preview-img");
+    // let imageData = document.createElement("img");
+    // document.getElementById("img-field").appendChild(imageData);
+    // imageData.classList.add("preview-img");
+    // imageData.setAttribute("id", "preview-img");
+
+    let image = document.getElementById("galeri-img");
     imageData.setAttribute("src", `./images/pictures/${i}.jpg`);
-    imageData.setAttribute("imgNumber", i);
-    let imageNumber = imageData.getAttribute("imgNumber") - 1;
+    // imageData.setAttribute("imgNumber", i);
+    // let imageNumber = imageData.getAttribute("imgNumber") - 1;
   }
 
   function hide() {
